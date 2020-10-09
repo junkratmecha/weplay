84c67e852809cb1462c10f4c93fc6dec1831ef3d
junkratmecha <junkratmecha@gmail.com>
2020-10-10 00:23:20 +0900

    device install and static page ready

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  
+  def home
+  end
+
+  def help
+  end
+  
+  def about
+  end
+  
+  def terms
+  end
+  
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  
+
+devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'   
+} 
+
+devise_scope :user do
+  get "user/:id", :to => "users/registrations#detail"
+  get "signup", :to => "users/registrations#new"
+  get "login", :to => "users/sessions#new"
+  get "logout", :to => "users/sessions#destroy"
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'pages#home'
+  get  '/help',    to: 'pages#help'
+  get  '/about',   to: 'pages#about'
+  get  '/terms',   to: 'pages#terms'
+
+
+
 end
