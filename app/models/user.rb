baaf85280a17dac9caf23b74c0fbf3b0bd1993c5
junkratmecha <junkratmecha@gmail.com>
2020-10-16 15:32:57 +0900

    user-icon by carriewave

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   enum sex: { 男性: 0, 女性: 1}
-  
+  mount_uploader :image, ImageUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
