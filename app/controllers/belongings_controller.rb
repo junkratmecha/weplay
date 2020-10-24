e28c3c42b13c9f6b3d8026793eea6012fed675ed
junkratmecha <junkratmecha@gmail.com>
2020-10-24 19:02:00 +0900

    reflect rubocop error

--- app/controllers/belongings_controller.rb
+++ app/controllers/belongings_controller.rb
@@ -1,22 +1,23 @@
 class BelongingsController < ApplicationController
 
   def create
-      @belonging = Belonging.create(clan_id: belonging_params[:clan_id], user_id: belonging_params[:user_id])
-      Request.find(belonging_params[:request_id]).destroy!
-      redirect_to clan_requests_url(@belonging.clan), notice:"「#{@belonging.user.name}」が、クラン：#{@belonging.clan.name}へ加入しました。"
+    @belonging = Belonging.create
+    (clan_id: belonging_params[:clan_id], user_id: belonging_params[:user_id])
+    Request.find(belonging_params[:request_id]).destroy!
+    redirect_to clan_requests_url(@belonging.clan),
+    notice: "「#{@belonging.user.name}」が、クラン：#{@belonging.clan.name}へ加入しました。"
   end
 
   def destroy
     @belonging = Belonging.find(params[:id])
     @belonging.destroy!
     @clan = Clan.find(params[:clan_id])
-    redirect_to clan_url(@clan), notice: "コミュニティ「#{@clan.name}」を退会しました。"    
-end
+    redirect_to clan_url(@clan), notice: "コミュニティ「#{@clan.name}」を退会しました。"
+  end
 
   private
 
   def belonging_params
-      params.permit(:clan_id, :user_id, :request_id)
+    params.permit(:clan_id, :user_id, :request_id)
   end
-
 end
--- spec/factories/clans.rb
+++ spec/factories/clans.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :clan do
-    
   end
 end
