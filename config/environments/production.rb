3b8b8357fc8af69f419e6b5c56f940ba5a3d8eed
junkratmecha <junkratmecha@gmail.com>
2020-11-03 16:55:14 +0900

    heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  # アップロードされたファイルをAWSに保存する
+  config.active_storage.service = :amazon
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
